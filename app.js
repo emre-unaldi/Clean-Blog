340ed5dce978bb1d81bfff81bf420357080b0c4a
emre-unaldi <emree.unaldi@gmail.com>
2022-09-12 19:47:13 +0300

    Development environment created

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    const blog = { 
+        id: 1, 
+        title: "Blog title", 
+        description: "Blog description" 
+    }
+    res.send(blog);
+})
+
+app.listen(PORT, () => {
+    console.log(`Sunucu ${PORT} portunda başlatıldı...`);
+});
