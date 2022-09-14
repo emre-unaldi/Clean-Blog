ff30705e827af534962acc7840ba3a14880c7f56
emre-unaldi <emree.unaldi@gmail.com>
2022-09-14 17:38:53 +0300

    Editing static files

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
 const PORT = 3001;
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// Middleware
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    const blog = { 
-        id: 1, 
-        title: "Blog title", 
-        description: "Blog description" 
-    }
-    res.send(blog);
-})
+    res.render('index')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+    res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+    res.render('post');
+});
 
 app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda başlatıldı...`);
