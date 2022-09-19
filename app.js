aa9c2aa3d5a23539f85401a8e175411872c0ad65
emre-unaldi <emree.unaldi@gmail.com>
2022-09-19 22:04:40 +0300

    Setting up mongodb connection and database edits

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Blog = require('./Models/Post');
+const Post = require('./Models/Post');
+
 const app = express();
 const PORT = 3001;
+
+// Connect DB
+mongoose.connect('mongodb://localhost/clean-blog', {
+    useNewUrlParser : true,
+    useUnifiedTopology : true 
+})
 
 // Template Engine
 app.set("view engine", "ejs");
 
 // Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const posts = await Post.find({});
+    res.render('index',{
+        posts
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -24,6 +39,11 @@ app.get('/add_post', (req, res) => {
 
 app.get('/post', (req, res) => {
     res.render('post');
+});
+
+app.post('/posts', async (req, res) => {
+    await Post.create(req.body);
+    res.redirect('/')
 });
 
 app.listen(PORT, () => {
